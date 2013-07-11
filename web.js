c097c1c126074ba8934c7fb50a9cb8217547541b
elisaa <elisaa1023@gmail.com>
2013-07-11 00:13:31 -0700

    modified

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buffer = fs.readFileSync('index.html')
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("ascii"));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
